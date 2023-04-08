ba56bf46e9e5181322dfb675f734e901531c9467
Ayush <19rajayush109@gmail.com>
2023-04-09 02:42:00 +0530

    dfs creating

--- Trees/dfs.java
+++ Trees/dfs.java
@@ -1,5 +1,60 @@
 package Trees;
 
+import java.util.*;
+
+class node{
+    int data;
+    node left;
+    node right;
+
+    public node(int key)
+    {
+        data = key;
+    }
+}
+
 public class dfs {
+
+    public static void main(String[] args) {
+
+        ArrayList <Integer>list=new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+    Queue<node>q= new LinkedList<>();
+
+
+
+
+        node a = new node(1);
+        node b = new node(2);
+        node c = new node(3);
+        node d = new node(4);
+        node e = new node(5);
+        node f = new node(6);
+
+        a.left = b;
+        a.right = c;
+        b.left=d;
+        b.right=e;
+        c.right=f;
+
+        q.add(a);
+
+   while(q.size()!=0)
+   {
+        node temp=q.remove();
+
+        if(temp.left!=null)
+            q.add(temp.left);
+
+        if(temp.right!=null)
+            q.add(temp.right);
+
+        list.add(temp.data);
+   }
+
+    System.out.println(list);
+}
     
 }
